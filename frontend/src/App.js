c4b55dde907af6f2f80983bba271175c6cdb3cd9
QDuy <duypq@pivot.jp>
2020-11-02 23:15:37 +0700

    add package

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,16 @@
-import React, { useState, useEffect} from 'react'
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 import './Styles/app.scss';
-import httpBoards from './Api/boards'
-import Card from 'react-bootstrap/Card';
-import util from './Utils'
 
 function App() {
-  const [boards, setBoards] = useState([]);
-  useEffect(() => {
-    async function fetchData() {
-      let result = await httpBoards.getListBoards()
-      console.log(result.data.boards)
-      setBoards(result.data.boards)
-    }
-    fetchData()
-  }, [])
 
   return (
     <div className="App">
-      <div className="list-card">
-        {boards.map(item => (
-          <Card key={item.id} className="card-item">
-            <Card.Body>
-              <Card.Title>{item.name}</Card.Title>
-              <Card.Text>công việc {item.id} của duy </Card.Text>
-              <div className="card-item__date">created: {util.formatDate(item.createdAt)}</div>
-            </Card.Body>
-          </Card>
-        ))}
-      </div>
+      
     </div>
   );
 }
